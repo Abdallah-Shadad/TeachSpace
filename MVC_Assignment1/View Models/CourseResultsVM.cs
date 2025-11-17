1f4dec1cf1a2d31d41c94448b8354d2c7c864d84
Abdallah Shadad <abdallhshadad8@gmail.com>
2025-11-17 14:50:02 +0200

    Edit Reslut Controller (Paging+ Async it)

--- MVC_Assignment1/View Models/CourseResultsVM.cs
+++ MVC_Assignment1/View Models/CourseResultsVM.cs
@@ -1,12 +1,13 @@
-﻿namespace MVC_Assignment1.View_Models
+﻿using X.PagedList;
+
+namespace MVC_Assignment1.View_Models
 {
     public class CourseResultsVM
     {
         public string CourseName { get; set; }
         public int MinDegree { get; set; }
-
         public string DepartmentName { get; set; }
-        public List<TraineeResultVM> Trainees { get; set; }
-    }
 
+        public IPagedList<TraineeResultVM> Trainees { get; set; }
+    }
 }
