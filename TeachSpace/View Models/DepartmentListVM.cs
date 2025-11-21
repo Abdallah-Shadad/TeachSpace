eadce2581babe2246debd491fa9d184bd4018ed4
Abdallah Shadad <abdallhshadad8@gmail.com>
2025-11-21 13:00:01 +0200

    Add Departments Controller and Views

---
+++ TeachSpace/View Models/DepartmentListVM.cs
@@ -0,0 +1,9 @@
+﻿namespace TeachSpace.View_Models
+{
+    public class DepartmentListVM
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string Manager { get; set; }
+    }
+}
