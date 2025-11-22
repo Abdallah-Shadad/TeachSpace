b222fd7e969f1b2d2a08e276e4e3883231331c58
Abdallah Shadad <abdallhshadad8@gmail.com>
2025-11-22 13:21:25 +0200

    add helpers

---
+++ TeachSpace/Helpers/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace TeachSpace.Helpers
+{
+    public static class FileHelper
+    {
+        public static async Task<string> SaveImageAsync(IFormFile file, string folder = "wwwroot/images")
+        {
+            if (file == null)
+                return "default.png";
+
+            Directory.CreateDirectory(folder);
+
+            string fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            string path = Path.Combine(folder, fileName);
+
+            using (var fs = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(fs);
+            }
+
+            return fileName;
+        }
+    }
+
+}
---
+++ TeachSpace/Helpers/SmartReturnHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeachSpace.Helpers
+{
+    public static class SmartReturnHelper
+    {
+        public static IActionResult GoBack(
+            Controller controller,
+            string returnTo,
+            int? deptId,
+            int? courseId)
+        {
+            return returnTo switch
+            {
+                "Department" => controller.RedirectToAction("Details", "Departments", new { id = deptId }),
+                "Course" => controller.RedirectToAction("Instructors", "Courses", new { id = courseId }),
+                _ => controller.RedirectToAction("Index")
+            };
+        }
+    }
+
+}
